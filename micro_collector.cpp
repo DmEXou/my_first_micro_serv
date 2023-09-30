30c4981b0ccae9e9bc05085c84cb223364e0c0f8
DmEXou <91079520+DmEXou@users.noreply.github.com>
2023-09-30 17:54:33 +0700

    Update micro_collector.cpp
    Add update page. (Добавлено обновление страницы каждые 3 часа по запросу(get)).

--- micro_collector.cpp
+++ micro_collector.cpp
@@ -1,20 +1,39 @@
-﻿#include <crow.h>
+#include <chrono>
+#include <ctime>
 
-#include "include/micro_parser_tuva_energo.h"
-#include "include/micro_proj_rates.h"
+#include <crow.h>
 
-int main() {
-	nlohmann::json final;
+#include "Include/micro_parser_tuva_energo.h"
+#include "Include/micro_proj_rates.h"
+
+void update_json(nlohmann::json& final) {
+	const auto now = std::chrono::system_clock::now();
+	const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+	std::string upd_time = std::ctime(&t_c);
+	upd_time.pop_back();
+	final["Time update"] = upd_time;
 	final["energy"] = json_energy::build();
 	Exchange_rates er;
 	final["rates"] = er.json_exchange_rates({"USD", "EUR"});
-	//std::cout << final.dump(4);
-    	crow::SimpleApp app;
+}
 
-    	CROW_ROUTE(app, "/")([&final]() {
-        	return final.dump(4);
+int main() {
+	nlohmann::json final;
+	update_json(final);
+    crow::SimpleApp app;
+	std::chrono::time_point<std::chrono::steady_clock> old_t_point = std::chrono::steady_clock::now();
+
+    CROW_ROUTE(app, "/")([&final, &old_t_point]() {
+		std::chrono::time_point<std::chrono::steady_clock> new_t_point = std::chrono::steady_clock::now();
+
+		if (std::chrono::duration_cast<std::chrono::hours>(new_t_point - old_t_point) >= std::chrono::hours(3)) {
+			update_json(final);
+			old_t_point = std::chrono::steady_clock::now();
+		}
+		//CROW_LOG_INFO << 
+        return final.dump(4);
         });
 
-    	app.port(80).multithreaded().run();
+    app.port(18080).multithreaded().run();
 	return 0;
 }
